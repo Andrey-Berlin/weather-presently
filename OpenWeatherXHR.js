9853422ce9f7dd5937385c18c0a7e69faaf4daa9
Андрей Берлин <99011598+Andrey-Berlin@users.noreply.github.com>
2022-02-15 22:39:05 +0200

    update OpenWeatherXHR.js

--- OpenWeatherXHR.js
+++ OpenWeatherXHR.js
@@ -18,20 +18,47 @@ button.onclick = function(){
             let results = JSON.parse(response);
             console.log(results);
             result.innerHTML =
-                `                
+                `   <hr />            
                     <h2>${results.name}</h2>
-                    
+                    <hr />
                    <ul>
-                     <li>Погода: ${results.weather[0].main}</li>
+                    <hr />
+                     <li>Погода: 
+                    
+                     ${results.weather[0].main}</li>
+                     <hr />
                      <li><img src="https://openweathermap.org/img/wn/${results.weather[0].icon}@2x.png" alt=""></li>
+                     <hr />
                      <li>Погода: ${results.weather[0].description}</li>
-                    <li>Облака: ${results.clouds.all}% (Норма: 0%)</li>
-                     <li>Температура: ${results.main.temp}C° (Норма для чел.: +20C°)</li>
-                    <li>Ощущается: ${results.main.feels_like}C° (Норма для чел.: +20C°)</li>
-                    <li>Влажность: ${results.main.humidity}% (Норма: 50%)</li>
-                    <li>Давление: ${results.main.pressure}hPa  (Норма: 1013 hPa)</li>
-                   <li>Ветер: ${results.wind.speed}m/s (Норма: до 1 м/с)</li>
+                    <hr />
+                    <li>Облака:
+                    ${results.clouds.all}% 
+                     <hr />
+                    (Норма: 0%)</li>
+                     <hr />
+                     <li>Температура:
+                     
+                     ${results.main.temp}C° 
+                      <hr />
+                     (Норма для чел.: +20C°)</li>
+                    <hr />
+                    <li>Ощущается: ${results.main.feels_like}C°
+                    <hr />
+                    (Норма для чел.: +20C°)</li>
+                    <hr />
+                    <li>Влажность: ${results.main.humidity}% 
+                    <hr />
+                    (Норма: 50%)</li>
+                    <hr />
+                    <li>Давление: ${results.main.pressure}hPa 
+                    <hr />
+                    (Норма: 1013 hPa)</li>
+                   <hr />
+                   <li>Ветер: ${results.wind.speed}m/s
+                     <hr />
+                    (Норма: до 1 м/с)</li>
                     </ul>
+                    <hr />
                   `
         }
     };
